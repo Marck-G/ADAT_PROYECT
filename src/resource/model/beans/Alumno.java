54c27d1bd4b125ebfa1ba4d8417afdf2276a4161
Marck <marckcarrion@gmail.com>
2018-10-01 12:10:05 +0200

    Crada la clase Libro

---
+++ src/resource/model/beans/Alumno.java
@@ -0,0 +1,121 @@
+package resource.model.beans;
+
+/**
+ * Representa el alumno almacenado en la base de datos
+ * @author Marck-G
+ *
+ */
+public class Alumno {
+	
+	private String dni;
+	private String nombre;
+	private String ap1;
+	private String ap2;
+	
+	/**
+	 * @param dni
+	 * @param nombre
+	 * @param ap1
+	 * @param ap2
+	 */
+	public Alumno(String dni, String nombre, String ap1, String ap2) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.ap1 = ap1;
+		this.ap2 = ap2;
+	}
+
+	/**
+	 * @return the dni
+	 */
+	public String getDni() {
+		return dni;
+	}
+
+	/**
+	 * @param dni the dni to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the ap1
+	 */
+	public String getAp1() {
+		return ap1;
+	}
+
+	/**
+	 * @param ap1 the ap1 to set
+	 */
+	public void setAp1(String ap1) {
+		this.ap1 = ap1;
+	}
+
+	/**
+	 * @return the ap2
+	 */
+	public String getAp2() {
+		return ap2;
+	}
+
+	/**
+	 * @param ap2 the ap2 to set
+	 */
+	public void setAp2(String ap2) {
+		this.ap2 = ap2;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alumno other = (Alumno) obj;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return ap1 + " " + ap2 +", "+ nombre;
+	}
+	
+}
--- src/resource/model/beans/Libro.java
+++ src/resource/model/beans/Libro.java
@@ -4,8 +4,8 @@
 package resource.model.beans;
 
 /**
+ *  Representa el libro que tenemos en la base de datos
  * @author Marck-G
- * Clase para el paso a objetos de la base relacional
  *
  */
 public class Libro {
@@ -14,6 +14,147 @@ public class Libro {
 	private String titulo;
 	private String autor;
 	private String editorial;
+	private String asignatura;
 	private Estado estado;
+	/**
+	 * Contructor
+	 * @param codigo
+	 * @param titulo
+	 * @param autor
+	 * @param editorial
+	 * @param asignatura
+	 * @param estado
+	 */
+	public Libro(String codigo, String titulo, String autor, String editorial, String asignatura, Estado estado) {
+		super();
+		this.codigo = codigo;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editorial = editorial;
+		this.asignatura = asignatura;
+		this.estado = estado;
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
 
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+	
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return titulo;
+	}
+
+
+
+	/**
+	 * @return the codigo
+	 */
+	public String getCodigo() {
+		return codigo;
+	}
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	/**
+	 * @return the titulo
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	/**
+	 * @return the autor
+	 */
+	public String getAutor() {
+		return autor;
+	}
+	/**
+	 * @param autor the autor to set
+	 */
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	/**
+	 * @return the editorial
+	 */
+	public String getEditorial() {
+		return editorial;
+	}
+	/**
+	 * @param editorial the editorial to set
+	 */
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	/**
+	 * @return the asignatura
+	 */
+	public String getAsignatura() {
+		return asignatura;
+	}
+	/**
+	 * @param asignatura the asignatura to set
+	 */
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
+	/**
+	 * @return the estado
+	 */
+	public Estado getEstado() {
+		return estado;
+	}
+	/**
+	 * @param estado the estado to set
+	 */
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	
+	
 }
