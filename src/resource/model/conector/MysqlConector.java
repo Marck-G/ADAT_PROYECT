ca2533ede6fa2c5ff516c22f1110388ad2038f31
Marck <marckcarrion@gmail.com>
2018-10-02 20:26:53 +0200

    Impelmentado MysqlConector

--- src/resource/model/conector/MysqlConector.java
+++ src/resource/model/conector/MysqlConector.java
@@ -1,5 +1,103 @@
 package resource.model.conector;
 
-public class MysqlConector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MysqlConector {
+		private static MysqlConector instance;
+		private String	url;
+		private String	username;
+		private String	pass;
+		private Connection connection;
+		
+		/**
+		 * 
+		 * @return instancia única de MysqlConector
+		 */
+		public static MysqlConector instance() {
+			return ( instance == null )? new MysqlConector(): instance;
+		}
+		
+		private MysqlConector() {}
+		
+		/**
+		 * establecemos los parametros necesarios
+		 * @param url
+		 * @param username
+		 * @param pass
+		 * @throws ClassNotFoundException
+		 * @throws SQLException
+		 */
+		public void setConectionData( String url, String username, String pass ) {
+			this.url = url;
+			this.username = username;
+			this.pass = pass;
+		}
+		/**
+		 * Conecta con la base de datos
+		 * @throws ClassNotFoundException
+		 * @throws SQLException
+		 */
+		public void conect() throws ClassNotFoundException, SQLException {
+			Class.forName( "com.mysql.jdbc.Driver" );
+			connection = DriverManager.getConnection( url, username, pass );
+		}
+		
+
+		/**
+		 * @return the url
+		 */
+		public String getUrl() {
+			return url;
+		}
+
+		/**
+		 * @param url the url to set
+		 */
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		/**
+		 * @return the username
+		 */
+		public String getUsername() {
+			return username;
+		}
+
+		/**
+		 * @param username the username to set
+		 */
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		/**
+		 * @return the pass
+		 */
+		public String getPass() {
+			return pass;
+		}
+
+		/**
+		 * @param pass the pass to set
+		 */
+		public void setPass(String pass) {
+			this.pass = pass;
+		}
+
+		/**
+		 * @return the connection
+		 */
+		public Connection getConnection() {
+			return connection;
+		}
+
+		
+		@Override
+		public String toString() {
+		return username+"|"+pass+"|"+url;
+		}
+		
 }
